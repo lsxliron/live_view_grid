35f10a6bc106ffc56dbf6c338bfb5d50347fcb05
lsxliron <lsxliron@noreply.com>
2023-12-31 14:57:11 -0500

    added docs

--- lib/live_view_grid/Behaviors/base_filter.ex
+++ lib/live_view_grid/Behaviors/base_filter.ex
@@ -1,4 +1,9 @@
+# cspell: ignore subquery
 defmodule LiveViewGrid.Behaviours.BaseFilter do
+  @moduledoc """
+  A simple behaviour to implement a filter. The `get_query` is being executed every time the grid want to get a certain query for a filter.
+  The `get_subquery` callback is being executed once for every condition in the filter.
+  """
   @callback get_query(struct(), String.t()) :: map() | nil
   @callback get_subquery(String.t(), String.t(), String.t()) :: map()
 end
